fb18eb1c986a10b118e4a1c4b58cc21363215a62
An Chaewoong <cwan0419@postech.ac.kr>
2025-02-26 16:34:43 +0900

    Update setup.bat
    delete useless icons

--- setup.bat
+++ setup.bat
@@ -1,11 +1,11 @@
 @echo off
 cd /d %~dp0
 echo =============================================
-echo 🚀 Setting up Python Virtual Environment...
+echo Setting up Python Virtual Environment...
 echo =============================================
 
 if not exist venv (
-    echo 🔧 Creating Virtual Environment...
+    echo Creating Virtual Environment...
     python -m venv venv
 )
 
@@ -13,13 +13,13 @@ echo activating virtual environment...
 call venv\Scripts\activate
 
 echo =============================================
-echo 📦 Installing required packages...
+echo Installing required packages...
 echo =============================================
 pip install --upgrade pip
 pip install -r requirements.txt
 
 echo =============================================
-echo ✅ Setup Complete! You can now run your program by run_program.bat or CMD.
+echo Setup Complete! You can now run your program by run_program.bat or CMD.
 echo =============================================
 
 echo deactivating virtual environment...
